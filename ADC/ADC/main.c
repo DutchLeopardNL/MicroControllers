c0b187efd4db984977ad860109f7124cf41267ac
Dutchie <jvgool2001@gmail.com>
2020-03-04 13:58:13 +0100

    bakkes pascal

---
+++ ADC/ADC/main.c
@@ -0,0 +1,18 @@
+/*
+ * ADC.c
+ *
+ * Created: 04/03/2020 01:56:02 PM
+ * Author : jvgoo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
